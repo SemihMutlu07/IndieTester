92786bd94f28fe6b2a93ee50c05c359d8298449f
croc <radixwashere@gmail.com>
2025-03-13 23:40:39 +0300

    Update
    Update

--- src/app/services/firebase.tsx
+++ src/app/services/firebase.tsx
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 //config
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: "indie-tester.firebaseapp.com",
-  projectId: "indie-tester",
-  storageBucket: "indie-tester.firebasestorage.app",
-  messagingSenderId: "952918757329",
-  appId: "1:952918757329:web:6789c25349e0bfdb2f1af0",
-  measurementId: "G-WM5MTPYNBK"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY as string,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN as string,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID as string,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID as string,
 };
 
 const app = initializeApp(firebaseConfig);
