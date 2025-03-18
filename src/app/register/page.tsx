01c7e23a5b004121fcfe9f7ca154b1351611a0d7
croc <radixwashere@gmail.com>
2025-03-18 18:42:45 +0300

    Create page.tsx

---
+++ src/app/register/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function DeveloperRole() {
+
+  return (
+    <div className='p-6'>
+      <h1 className='text-2xl font-bold mb-4'>Developer Dashboard</h1>
+      <p>Manage your games here.</p>
+      <a href="/developer/add" className='bg-blue-500 text-white px-4 py-2 rounded mt-4 inline-block'>
+        Add New Game
+      </a>
+    </div>
+  );
+}
